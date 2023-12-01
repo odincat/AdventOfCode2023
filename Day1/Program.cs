6c3ea802a00a9650bbb668f840cac4500021a863
odincat <70816143+odincat@users.noreply.github.com>
2023-12-01 19:52:45 +0100

    initial & day 1 complete

---
+++ Day1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+using Spectre.Console;
+
+Dictionary<string, int> wordNumbers = new() {
+    { "one", 1 },
+    { "two", 2 },
+    { "three", 3 },
+    { "four", 4 },
+    { "five", 5 },
+    { "six", 6 },
+    { "seven", 7 },
+    { "eight", 8 },
+    { "nine", 9 }
+};
+
+var fileLines = File.ReadAllLines("./input.txt");
+
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+
+int totalSum = 0;
+
+foreach(var lineString in fileLines) {
+    List<int> numbers = new();
+    List<string> characterBuffer = new();
+
+    foreach(char character in lineString) {
+        if (Char.IsNumber(character)) {
+            numbers.Add(int.Parse(character.ToString()));
+
+            characterBuffer.Clear();
+        } else {
+            characterBuffer.Add(character.ToString());
+        }
+
+        var joinedChars = string.Join("", characterBuffer);
+        var matchingNumber = wordNumbers.Where(w => joinedChars.Contains(w.Key)).FirstOrDefault();
+
+        if (matchingNumber.Key != null) {
+            numbers.Add(matchingNumber.Value);
+
+            characterBuffer.Clear();
+
+            // hack to still detect strings like 'sevenine' properly as 7 and 9
+            characterBuffer.Add(character.ToString());
+        }
+    }
+
+
+    int firstNum = numbers.FirstOrDefault();
+    int lastNum = numbers.LastOrDefault();
+
+    var joined = int.Parse($"{firstNum}{lastNum}");
+
+    totalSum += joined;
+}
+
+stopwatch.Stop();
+
+AnsiConsole.MarkupLine($"[blue]Result of all joined numbers is[/] [gray]'[/][lime]{totalSum}[/][gray]'[/] [gray]-[/] [blue]took[/] [lime]{stopwatch.ElapsedMilliseconds}ms[/]");
---
+++ readme.md
@@ -0,0 +1,11 @@
+# My Advent of Code Journey - 2023
+This is my first time participating. To be honest, I have a lot of respect for algorithms.
+So, let's see how this goes. Even though I try to solve these every day, it's probably not going to happen.
+
+## Language
+C# because it's a beautiful language. The only thing I miss from Rust is the match operator and compile-time macros for including files.
+I know some people use Advent of Code to push themselves out of their comfort zone and learn new languages, but this year I would just be happy to solve the puzzles at all.
+
+I also don't desperately try to push for optimization. If it runs pretty fast, I'm fine with it.
+
+Details: `.NET version: 8.0.100 - C# version 12`
